cdba021a39c5535bdcd3ed1cdeb831554051cd00
mehdi dridi <mehdignu@users.noreply.github.com>
2019-01-01 15:02:46 +0100

    styling the landing page

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import InputFilter from '../../components/UI/InputFilter/InputFilter';
 import {connect} from 'react-redux';
 import * as actiontypes from '../../store/actions';
-import Aux from '../../hoc/Aux/Aux';
+import classes from './Search.css';
+
 
 class search extends Component {
 
@@ -26,16 +27,33 @@ class search extends Component {
 
 
         let form = (
-            <form onSubmit={this.handleSubmit}>
+
+            <form onSubmit={this.handleSubmit} className={classes["search-form"]}>
                 <InputFilter inputtype="input" type="text" name="name"
                              placeholder="player name / accountID" onChange={this.inputChangedHandler}/>
             </form>
         );
 
         return (
-            <Aux>
-                {form}
-            </Aux>
+            <header className={classes.header}>
+
+
+                <div className={classes["text-box"]}>
+                    <img src="https://via.placeholder.com/150" alt="Logo" className={classes.logo}/>
+
+                    <h1 className={classes["heading-primary"]}>
+                        <span className={classes["heading-primary--main"]}>Dota2 stats</span>
+                        <span className={classes["heading-primary--sub"]}>Advanced analytics platform</span>
+
+                    </h1>
+
+                    {form}
+
+
+                </div>
+
+
+            </header>
         );
     }
 }
